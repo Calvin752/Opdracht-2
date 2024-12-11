3b7c9cdfe91d41f2c6fbfa9000a3208cc15806bc
Calvin752 <raijinsan2@gmail.com>
2024-12-11 12:53:26 +0100

    Add files via upload

---
+++ js2/main.js
@@ -0,0 +1,50 @@
+function createTweet(userName, content) {
+    return {
+        id: Date.now(),
+        username: username,
+        content: content,
+        likes: 0,
+        lastEdited: new Date(),
+
+        like: function() {
+            this.likes++;
+        },
+
+        edit: function(newContent) {
+            this.content = newContent;
+            this.lastEdited = new Date();
+        },
+
+        toHTML: function() {
+            return `div class="tweet" data-id="${this.id}">
+            <p>${this.content}</p>
+            <p>${this.likes} likes</p>
+            <button class="like-btn">Like</button>
+            <button class="edit-btn">Edit</button>
+            <button class="delete-btn">Delete</button>
+            </div>`;
+        }
+    };  
+}
+
+const tweetManager = {
+    tweets: [],
+
+    addTweet: function(tweet) {
+        this.tweets = this.tweets.filter(tweet => tweet.id !== id);
+        this.saveToLocalStorage();
+    },
+
+    getTweetById: function(id) {
+        return this.tweets.find(tweet => tweet.id === id);
+    },
+
+    saveToLocalStorage: function() {
+        localStorage.setItem('tweets', JSON.stringify(this.tweets));
+    },
+
+    loadFromLocalStorage: function() {
+        const storedTweets = JSON.parse(localStorage.getItem('tweets') || "[]");
+        this.tweets = storedTweets.map(data => createTweet(data.username, data.content));
+    }
+};
